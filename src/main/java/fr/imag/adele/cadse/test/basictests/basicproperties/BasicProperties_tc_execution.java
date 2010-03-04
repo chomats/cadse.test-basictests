286e8be0a5570be6c68da4278c1a209da7027b0a
etienne <etienne@localhost>
2010-03-04 14:11:48 +0100

    update on basic test execution part

--- src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_tc_execution.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_tc_execution.java
@@ -116,6 +116,23 @@ public class BasicProperties_tc_execution extends GTCadseTestCase {
 	}
 
 	/**
+	 * Tests enum basic properties.
+	 * <ul>
+	 * <li>name</li>
+	 * <li>default value</li>
+	 * <li>hidden in computed pages</li>
+	 * <li>must be initialized</li>
+	 * <li>is list</li>
+	 * </ul>
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void test_enum_basic_properties() throws Exception {
+		enumAttributeTest("my_enum_type", "my_enum_instance", "two", "", "three", "three");
+	}
+
+	/**
 	 * A common method used by all attributes tests.
 	 * 
 	 * @param typeName
@@ -145,21 +162,13 @@ public class BasicProperties_tc_execution extends GTCadseTestCase {
 		GTCadseFactory.findCadseField(shell, CadseGCST.ITEM_at_NAME_).typeText(instanceName);
 
 		// Default value
+		assertEquals(emptyValue, GTCadseFactory.findCadseField(shell, "no_default_value").getValue());
 		assertEquals(defaultValue, GTCadseFactory.findCadseField(shell, "with_default_value").getValue());
 
-		// Hidden in computed pages and must be initialized
-		GTCadseFactory.findCadseField(shell, "notHid_beInit").setValue(newValue);
-		assertCadseFieldDoesNotExist(shell, "notHid_notInit", failingAssertTimeout);
-		assertCadseFieldDoesNotExist(shell, "hid_notInit", failingAssertTimeout);
-		assertCadseFieldDoesNotExist(shell, "hid_beInit", failingAssertTimeout);
-
-		// List
-		shell.findButton("Add...").click();
-		GTShell shell2 = new GTShell("");
-		shell2.findText().typeText(listValue);
-		shell2.close();
-
-		GTCadseFactory.findCadseField(shell, "not_list").setValue(newValue);
+		// Hidden in computed pages
+		// Must be initialized
+		// Is List
+		commonCreationHiddenMustBeIsList(shell, listValue, newValue);
 
 		shell.close();
 
@@ -176,6 +185,101 @@ public class BasicProperties_tc_execution extends GTCadseTestCase {
 		// Default value
 		assertEquals(emptyValue, GTCadseFactory.findCadseField(propertiesView, "no_default_value").getValue());
 		assertEquals(defaultValue, GTCadseFactory.findCadseField(propertiesView, "with_default_value").getValue());
+
+		// Hidden in computed pages
+		// Must be initialized
+		// Is List
+		commonModificationHiddenMustBeIsList(listValue, newValue);
+	}
+
+	/**
+	 * Performs a set of tests on the enum attribute.
+	 * 
+	 * @param typeName
+	 *            the name of the item type.
+	 * @param instanceName
+	 *            the name for the new instance.
+	 * @param defaultValue
+	 *            the default value which should be found
+	 * @param emptyValue
+	 *            the value to match an empty value
+	 * @param listValue
+	 *            a value to be added in a list
+	 * @param newValue
+	 *            a new value
+	 */
+	private void enumAttributeTest(String typeName, String instanceName, Object defaultValue, Object emptyValue,
+			String listValue, Object newValue) {
+
+		/* ============== */
+		/* Creation page */
+		/* ============== */
+
+		workspaceView.contextMenuNew(typeName).click();
+		GTShell shell = new GTShell(typeName);
+
+		// name
+		GTCadseFactory.findCadseField(shell, CadseGCST.ITEM_at_NAME_).typeText(instanceName);
+
+		// Hidden in computed pages
+		// Must be initialized
+		// Is List
+		commonCreationHiddenMustBeIsList(shell, listValue, newValue);
+
+		shell.close();
+
+		/* ============= */
+		/* Property page */
+		/* ============= */
+
+		workspaceView.selectNode(instanceName);
+		propertiesView.showTab(typeName);
+
+		// Name
+		assertEquals(instanceName, GTCadseFactory.findCadseField(propertiesView, CadseGCST.ITEM_at_NAME_).getText());
+
+		// Hidden in computed pages
+		// Must be initialized
+		// Is List
+		commonModificationHiddenMustBeIsList(listValue, newValue);
+	}
+
+	/**
+	 * Performs tests with Hidden in computed pages, must be initialized and isList attributes in creation pages.
+	 * 
+	 * @param listValue
+	 *            the list value
+	 * @param newValue
+	 *            the new value
+	 * @param shell
+	 *            the shell
+	 */
+	private void commonCreationHiddenMustBeIsList(GTShell shell, String listValue, Object newValue) {
+
+		// Hidden in computed pages and must be initialized
+		GTCadseFactory.findCadseField(shell, "notHid_beInit").setValue(newValue);
+		assertCadseFieldDoesNotExist(shell, "notHid_notInit", failingAssertTimeout);
+		assertCadseFieldDoesNotExist(shell, "hid_notInit", failingAssertTimeout);
+		assertCadseFieldDoesNotExist(shell, "hid_beInit", failingAssertTimeout);
+
+		// List
+		shell.findButton("Add...").click();
+		GTShell shell2 = new GTShell("");
+		shell2.findText().typeText(listValue);
+		shell2.close();
+
+		GTCadseFactory.findCadseField(shell, "not_list").setValue(newValue);
+	}
+
+	/**
+	 * Performs tests with Hidden in computed pages, must be initialized and isList attributes in modification pages.
+	 * 
+	 * @param listValue
+	 *            a value to be found in the list
+	 * @param newValue
+	 *            a new value
+	 */
+	private void commonModificationHiddenMustBeIsList(String listValue, Object newValue) {
 
 		// Hidden in computed pages and must be initialized
 		assertEquals(newValue, GTCadseFactory.findCadseField(propertiesView, "notHid_beInit").getValue());
