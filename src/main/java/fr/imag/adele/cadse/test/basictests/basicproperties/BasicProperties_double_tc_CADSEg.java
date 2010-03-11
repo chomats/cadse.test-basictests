defb608998a682ec00f1753b336c7ce8c03ad130
etienne <etienne@localhost>
2010-03-11 17:32:32 +0100

    The Double attribute Test

--- src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_double_tc_CADSEg.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_double_tc_CADSEg.java
@@ -2,10 +2,7 @@ package fr.imag.adele.cadse.test.basictests.basicproperties;
 
 import org.junit.Test;
 
-import fr.imag.adele.cadse.cadseg.managers.CadseDefinitionManager;
-import fr.imag.adele.cadse.core.CadseGCST;
 import fr.imag.adele.graphictests.cadse.test.GTCadseRTConstants;
-import fr.imag.adele.graphictests.gttree.GTTreePath;
 
 /**
  * Tests basic properties for double attributes.
@@ -16,12 +13,7 @@ import fr.imag.adele.graphictests.gttree.GTTreePath;
  * <li>cannot be undefined</li>
  * </ul>
  */
-public class BasicProperties_double_tc_CADSEg extends BasicProperties_number_tc_CADSEg {
-
-	protected final String attr_name = "double";
-	protected final String cadse_name = "CADSE_BasicProperties_" + attr_name;
-	protected final GTTreePath cadse_model = new GTTreePath(cadse_name);
-	protected final GTTreePath data_model = cadse_model.concat(CadseDefinitionManager.DATA_MODEL);
+public class BasicProperties_double_tc_CADSEg extends BasicProperties_number_tc_Common {
 
 	/**
 	 * Makes a few things before the test starts.
@@ -43,9 +35,6 @@ public class BasicProperties_double_tc_CADSEg extends BasicProperties_number_tc_
 		// Closes unless views
 		welcomeView.close();
 		workspaceView.show();
-
-		// Creates a new CADSE
-		createCadseDefinition(cadse_name, "model." + cadse_name);
 	}
 
 	/**
@@ -56,12 +45,7 @@ public class BasicProperties_double_tc_CADSEg extends BasicProperties_number_tc_
 	 */
 	@Test
 	public void test_double() throws Exception {
-
-		String[] defVal = { null, "123" };
-		boolean[] sicp = { true, false };
-		boolean[] simp = { true, false };
-		boolean[] cbu = { true, false };
-
-		createItems(data_model, CadseGCST.DOUBLE, attr_name, defVal, sicp, simp, cbu);
+		BasicProperties_double_data data = new BasicProperties_double_data();
+		data.createAll();
 	}
 }
--- src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_double_tc_execution.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_double_tc_execution.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 /**
  */
-public class BasicProperties_double_tc_execution extends BasicProperties_number_tc_execution {
+public class BasicProperties_double_tc_execution extends BasicProperties_number_tc_Common {
 
 	/**
 	 * Selects the CADSE to be executed and closes unless views.
@@ -20,30 +20,7 @@ public class BasicProperties_double_tc_execution extends BasicProperties_number_
 	@Test
 	public void test_double() throws Exception {
 
-		int instanceNumber = 1;
-		for (int typeNumber = 1; typeNumber <= 16; typeNumber++) {
-			System.out.println("Starting Test Double part 1 : instance#=" + instanceNumber + " type#=" + typeNumber
-					+ " New visual=456.0");
-			doubleTest(instanceNumber++, typeNumber, "456.0", 456d);
-			System.out.println("Starting Test Double part 2 : instance#=" + instanceNumber + " type#=" + typeNumber
-					+ " New visual=\"\"");
-			doubleTest(instanceNumber++, typeNumber, "", null);
-		}
-	}
-
-	private void doubleTest(int instanceNumber, int typeNumber, String newVisualValue, Double newModelValue) {
-
-		// The type name
-		String type = "double";
-
-		// A not null default value
-		String initialVisualValueNotNull = "123.0";
-
-		// The model value, corresponding to the initialVisualValueNotNull
-		Object initialModelValueNotNull = 123d;
-
-		// Performs the test
-		computeParameters(type, instanceNumber, typeNumber, initialVisualValueNotNull, initialModelValueNotNull,
-				newVisualValue, newModelValue);
+		BasicProperties_double_data data = new BasicProperties_double_data();
+		data.testExecution();
 	}
 }
