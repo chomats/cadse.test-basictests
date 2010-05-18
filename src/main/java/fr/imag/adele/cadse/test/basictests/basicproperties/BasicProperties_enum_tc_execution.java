757152270dc6f32104886a02063a228d7e0cded6
etienne <etienne@localhost>
2010-05-18 14:18:03 +0200

    update for selecting cadses

--- src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_enum_tc_execution.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/basicproperties/BasicProperties_enum_tc_execution.java
@@ -1,7 +1,6 @@
 package fr.imag.adele.cadse.test.basictests.basicproperties;
 
 import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.GTCadseView.workspaceView;
-import static fr.imag.adele.graphictests.cadse.test.GTCadseHelperMethods.selectCadses;
 import static fr.imag.adele.graphictests.gtworkbench_part.GTView.welcomeView;
 
 import org.junit.Test;
@@ -20,15 +19,13 @@ public class BasicProperties_enum_tc_execution extends GTCadseTestCase {
 	 */
 	@Test
 	public void test_preparation() throws Exception {
-
-		selectCadses("Cadse Model.Workspace." + driver.cadseName);
+		driver.selectCadse();
 		welcomeView.close();
 		workspaceView.show();
 	}
 
 	@Test
 	public void test_enum() throws Exception {
-
 		driver.testExecution();
 	}
 }
