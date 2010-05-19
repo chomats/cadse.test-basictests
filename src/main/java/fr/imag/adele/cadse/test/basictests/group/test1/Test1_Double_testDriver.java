cb90c5564e9c02d7135fb3c5fd0c9d41e4c69279
etienne <etienne@localhost>
2010-05-19 14:26:42 +0200

    Group : New tests (starting implementation)

---
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_Double_testDriver.java
@@ -0,0 +1,38 @@
+package fr.imag.adele.cadse.test.basictests.group.test1;
+
+import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.core.ItemType;
+import fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue;
+
+public class Test1_Double_testDriver extends Test1_common_testDriver {
+
+	/**
+	 * Instantiates a new test1_ integer_test driver.
+	 */
+	public Test1_Double_testDriver() {
+		defaultValue = new KeyValue(new String(), 10, 10);
+		newValue1 = new KeyValue(new String(), 11, 11);
+		newValue2 = new KeyValue(new String(), 12, 12);
+		newValue3 = new KeyValue(new String(), 13, 13);
+
+		initializeTables();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.imag.adele.cadse.test.basictests.group.test1.Test1_common_testDriver#getItemTypeUnderTest()
+	 */
+	@Override
+	protected ItemType getItemTypeUnderTest() {
+		return CadseGCST.DOUBLE;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.imag.adele.cadse.test.basictests.testdriver.GTCommonTestDriver#getTypeNameUnderTest()
+	 */
+	@Override
+	protected String getTypeNameUnderTest() {
+		return "Double";
+	}
+}
---
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_Long_ts_execution.java
@@ -0,0 +1,16 @@
+package fr.imag.adele.cadse.test.basictests.group.test1;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(value = { Test1_Long_tc_execution.class })
+public class Test1_Long_ts_execution {
+	public static Test suite() {
+		return new JUnit4TestAdapter(Test1_Long_ts_execution.class);
+	}
+}
