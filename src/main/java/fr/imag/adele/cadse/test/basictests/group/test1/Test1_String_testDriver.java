81d795195eb4ba954f42c0032aa7bb4a5ece4bd4
Etienne Gandrille <etienne.gandrille@imag.fr>
2010-06-18 16:40:08 +0200

    Update for String attribute

--- src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_String_testDriver.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_String_testDriver.java
@@ -1,5 +1,7 @@
 package fr.imag.adele.cadse.test.basictests.group.test1;
 
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.GTCadseView.propertiesView;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.GTCadseView.workspaceView;
 import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.cbuKv;
 import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.listKv;
 import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notCbuKv;
@@ -11,6 +13,7 @@ import fr.imag.adele.cadse.core.CadseGCST;
 import fr.imag.adele.cadse.core.ItemType;
 import fr.imag.adele.cadse.test.basictests.testdriver.GTTestParameter;
 import fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue;
+import fr.imag.adele.graphictests.gttree.GTTreePath;
 
 public class Test1_String_testDriver extends Test1_Common_testDriver {
 
@@ -125,6 +128,43 @@ public class Test1_String_testDriver extends Test1_Common_testDriver {
 				return false;
 			}
 
+			return true;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * fr.imag.adele.cadse.test.basictests.testdriver.GTCommonTestDriver#postCreate(fr.imag.adele.cadse.test.basictests
+	 * .testdriver.GTTestParameter, fr.imag.adele.graphictests.gttree.GTTreePath,
+	 * fr.imag.adele.graphictests.gttree.GTTreePath)
+	 */
+	@Override
+	protected void postCreate(GTTestParameter tp, GTTreePath itPath, GTTreePath attrPath) {
+		workspaceView.selectNode(attrPath);
+		propertiesView.showTab("String");
+		propertiesView.setValue(tp.getValue("notEmpty"));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * fr.imag.adele.cadse.test.basictests.testdriver.GTCommonTestDriver#isSettableValue(fr.imag.adele.cadse.test.basictests
+	 * .testdriver.GTTestParameter, fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue)
+	 */
+	@Override
+	protected boolean isSettableValue(GTTestParameter tp, KeyValue kv) {
+
+		boolean isList = tp.getBoolean("list");
+		boolean notEmpty = tp.getBoolean("notEmpty");
+
+		if (isList) {
+			if (kv.value == null)
+				return false;
+			else
+				return notEmpty ? !kv.getString().equals("") : true;
+		}
+		else {
 			return true;
 		}
 	}
