d89f358820843a432d06d28257bc55676b67734b
Etienne Gandrille <etienne.gandrille@imag.fr>
2010-06-21 16:18:20 +0200

    Boolean update

--- src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_Boolean_testDriver.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_Boolean_testDriver.java
@@ -115,4 +115,24 @@ public class Test1_Boolean_testDriver extends Test1_Common_testDriver {
 			}
 		}
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * fr.imag.adele.cadse.test.basictests.testdriver.GTCommonTestDriver#isAttributeCreationSuccess(fr.imag.adele.cadse
+	 * .test.basictests.testdriver.GTTestParameter)
+	 */
+	@Override
+	protected boolean isAttributeCreationSuccess(GTTestParameter tp) {
+		boolean cbu = tp.getBoolean("cbu");
+		Boolean defVal = tp.getBoolean("defVal");
+		boolean isList = tp.getBoolean("list");
+
+		if (defVal == null && cbu == true && !isList) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 }
