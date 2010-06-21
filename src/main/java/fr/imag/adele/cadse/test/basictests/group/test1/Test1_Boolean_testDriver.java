bbbdfc6eb487fcc6aa248d228838669310db7c06
Etienne Gandrille <etienne.gandrille@imag.fr>
2010-06-21 16:12:52 +0200

    Boolean is settable value

--- src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_Boolean_testDriver.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test1/Test1_Boolean_testDriver.java
@@ -9,6 +9,7 @@ import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.si
 import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.simpKv;
 import fr.imag.adele.cadse.core.CadseGCST;
 import fr.imag.adele.cadse.core.ItemType;
+import fr.imag.adele.cadse.test.basictests.testdriver.GTTestParameter;
 import fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue;
 
 public class Test1_Boolean_testDriver extends Test1_Common_testDriver {
@@ -88,5 +89,30 @@ public class Test1_Boolean_testDriver extends Test1_Common_testDriver {
 	@Override
 	protected String getAttributeNameUnderTest() {
 		return "Boolean";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * fr.imag.adele.cadse.test.basictests.testdriver.GTCommonTestDriver#isSettableValue(fr.imag.adele.cadse.test.basictests
+	 * .testdriver.GTTestParameter, fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue)
+	 */
+	@Override
+	protected boolean isSettableValue(GTTestParameter tp, KeyValue kv) {
+
+		boolean isList = tp.getBoolean("list");
+		boolean cbu = tp.getBoolean("cbu");
+
+		if (isList) {
+			return kv != null && kv.value != null && !kv.value.toString().equals("");
+		}
+		else {
+			if (cbu && kv != null && kv.value == null) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
 	}
 }
