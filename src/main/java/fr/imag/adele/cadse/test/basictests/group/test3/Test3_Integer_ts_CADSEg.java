f195b2227d5e6527f6d91e6e775ca497adb6c66e
Etienne Gandrille <etienne.gandrille@imag.fr>
2010-07-01 10:43:45 +0200

    Test3

--- src/main/java/fr/imag/adele/cadse/test/basictests/group/test3/Test3_Integer_ts_CADSEg.java
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test3/Test3_Integer_ts_CADSEg.java
@@ -1,5 +1,16 @@
 package fr.imag.adele.cadse.test.basictests.group.test3;
 
-public class Test3_Integer_ts_CADSEg {
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(value = { Test3_Integer_tc_CADSEg.class })
+public class Test3_Integer_ts_CADSEg {
+	public static Test suite() {
+		return new JUnit4TestAdapter(Test3_Integer_ts_CADSEg.class);
+	}
 }
