2058440a40f85b5699524a471c3d17433b9cdc58
Etienne Gandrille <etienne.gandrille@imag.fr>
2010-10-01 10:44:51 +0200

    Test2 Double implem - bug fix with cadse name

---
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test2/Test2_Common_parameters.java
@@ -0,0 +1,17 @@
+package fr.imag.adele.cadse.test.basictests.group.test2;
+
+import fr.imag.adele.cadse.test.basictests.common.GTCommonParameters;
+
+public abstract class Test2_Common_parameters extends GTCommonParameters {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.imag.adele.cadse.test.basictests.common.GTCommonParameters#getTestName
+	 * ()
+	 */
+	@Override
+	protected String getTestName() {
+		return "group_test2";
+	}
+}
