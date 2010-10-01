6ad50f076bcc21e690c37adfc0af2ff2251fd5ab
Etienne Gandrille <etienne.gandrille@imag.fr>
2010-10-01 14:47:31 +0200

    Test2

---
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test2/Test2_Boolean_parameters.java
@@ -0,0 +1,70 @@
+package fr.imag.adele.cadse.test.basictests.group.test2;
+
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.cbuKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.listKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notCbuKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notListKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notSicpKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notSimpKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.sicpKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.simpKv;
+import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.test.basictests.common.GTCollectionTestParameter;
+import fr.imag.adele.cadse.test.basictests.common.type.GTBooleanParameter;
+import fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue;
+
+public class Test2_Boolean_parameters extends Test2_Common_parameters {
+
+	/** Constructor. */
+	public Test2_Boolean_parameters() {
+		typeParameter = new GTBooleanParameter();
+		ctp = initCTP();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.imag.adele.cadse.test.basictests.common.GTCommonParameters#initCTP()
+	 */
+	@Override
+	protected GTCollectionTestParameter initCTP() {
+
+		GTCollectionTestParameter ctp = new GTCollectionTestParameter(typeParameter, getAttributeName(), getTestName());
+
+		/* =========== */
+		/* DEFINITIONS */
+		/* =========== */
+
+		/* Common parameters */
+		KeyValue[] sicpValues = { sicpKv(), notSicpKv() };
+		KeyValue[] simpValues = { simpKv(), notSimpKv() };
+		KeyValue[] cbuValues = { cbuKv(), notCbuKv() };
+		KeyValue[] listValues = { notListKv(), listKv() };
+
+		/* Values given into CADSEg */
+		KeyValue kv11 = new KeyValue(CadseGCST.ATTRIBUTE_at_DEFAULT_VALUE_, Boolean.TRUE);
+		KeyValue kv12 = new KeyValue(CadseGCST.ATTRIBUTE_at_DEFAULT_VALUE_, Boolean.FALSE);
+		KeyValue kv13 = new KeyValue(CadseGCST.ATTRIBUTE_at_DEFAULT_VALUE_, null);
+		KeyValue[] defVal = new KeyValue[] { kv11, kv12, kv13 };
+
+		/* Execution : new value */
+		KeyValue kv21 = new KeyValue(getAttributeName(), Boolean.TRUE);
+		KeyValue kv22 = new KeyValue(getAttributeName(), Boolean.FALSE);
+		KeyValue kv23 = new KeyValue(getAttributeName(), null);
+		KeyValue[] newVal = new KeyValue[] { kv21, kv22, kv23 };
+
+		/* ==== */
+		/* INIT */
+		/* ==== */
+
+		ctp.addParameter("sicp", sicpValues);
+		ctp.addParameter("simp", simpValues);
+		ctp.addParameter("cbu", cbuValues);
+		ctp.addParameter("list", listValues);
+		ctp.addParameter("defVal", defVal);
+		ctp.addParameter("newValue", newVal);
+
+		return ctp;
+	}
+}
---
+++ src/main/java/fr/imag/adele/cadse/test/basictests/group/test2/Test2_Enum_parameters.java
@@ -0,0 +1,83 @@
+package fr.imag.adele.cadse.test.basictests.group.test2;
+
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.listKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notListKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notSicpKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.notSimpKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.sicpKv;
+import static fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue.simpKv;
+import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.test.basictests.basicproperties.BasicProperties_Common_parameters;
+import fr.imag.adele.cadse.test.basictests.common.GTCollectionTestParameter;
+import fr.imag.adele.cadse.test.basictests.common.type.GTEnumParameter;
+import fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.KeyValue;
+import fr.imag.adele.graphictests.gttree.GTTreePath;
+
+public class Test2_Enum_parameters extends BasicProperties_Common_parameters {
+
+	/** Enum type name. */
+	String enumTypeName = "myEnum";
+
+	/** Constructor */
+	public Test2_Enum_parameters() {
+		typeParameter = new GTEnumParameter();
+		ctp = initCTP();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.imag.adele.cadse.test.basictests.common.GTCommonParameters#initCTP()
+	 */
+	@Override
+	protected GTCollectionTestParameter initCTP() {
+
+		GTCollectionTestParameter ctp = new GTCollectionTestParameter(typeParameter, getAttributeName(), getTestName());
+
+		/* =========== */
+		/* DEFINITIONS */
+		/* =========== */
+
+		/* Common parameters */
+		KeyValue[] sicpValues = { sicpKv(), notSicpKv() };
+		KeyValue[] simpValues = { simpKv(), notSimpKv() };
+		/*
+		 * KeyValue[] cbuValues = { cbuKv(), notCbuKv() }; CBU = this attribute
+		 * does not exists with enum type
+		 */
+		KeyValue[] listValues = { notListKv(), listKv() };
+
+		/* Default Value */
+		KeyValue kv11 = new KeyValue(CadseGCST.ATTRIBUTE_at_DEFAULT_VALUE_, "");
+		KeyValue kv12 = new KeyValue(CadseGCST.ATTRIBUTE_at_DEFAULT_VALUE_, "two");
+		KeyValue[] defVal = new KeyValue[] { kv11, kv12 };
+
+		/* Execution : new value */
+		KeyValue kv21 = new KeyValue(getAttributeName(), "");
+		KeyValue kv22 = new KeyValue(getAttributeName(), "three");
+		KeyValue[] newVal = new KeyValue[] { kv21, kv22 };
+
+		/* Enum Type */
+		KeyValue enumTypeKv = new KeyValue(CadseGCST.ENUM_lt_ENUM_TYPE, new GTTreePath(enumTypeName));
+		KeyValue[] enumType = new KeyValue[] { enumTypeKv };
+
+		/* ==== */
+		/* INIT */
+		/* ==== */
+
+		ctp.addParameter("sicp", sicpValues);
+		ctp.addParameter("simp", simpValues);
+		/*
+		 * ctp.addParameter("cbu", cbuValues); CBU = this attribute does not
+		 * exists with enum type
+		 */
+		ctp.addParameter("list", listValues);
+
+		ctp.addParameter("defVal", defVal);
+		ctp.addParameter("newValue", newVal);
+		ctp.addParameter("enumType", enumType);
+
+		return ctp;
+	}
+}
